bba783058b5f0e35adc73e3690b4e5e066356e65
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-28 23:25:34 +0700

    define routes respectively with each page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
+import About from "./components/About";
 import Home from "./components/Home";
+import Projects from "./components/Projects";
 import DefaultLayout from "./layouts/DefaultLayout";
 
 // not sign into this website
-const publicRoutes = [{ path: "/", component: Home, layout: DefaultLayout }];
+const publicRoutes = [
+  { path: "/", component: Home, layout: DefaultLayout },
+  { path: "/about-me", component: About, layout: DefaultLayout },
+  { path: "/my-projects", component: Projects, layout: DefaultLayout },
+];
 
 const privateRoutes = [];
 
