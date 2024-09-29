8d2d1f12185023f7187f31582caffbab061e7573
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-29 19:18:25 +0700

    prepare data for skill

---
+++ src/data/skillsData.js
@@ -0,0 +1,71 @@
+export const languagesData = [
+  {
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+    name: "Javascript",
+  },
+  {
+    image: "https://believeit.fr/fichiers/TypeScript.jpg",
+    name: "Typescript",
+  },
+];
+
+export const databaseData = [
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2ZYtHv2OLXmthRPbkmENZRXuqBVDwlsrZ1A&s",
+    name: "MongoDB",
+  },
+];
+
+export const frontEndDevelopments = [
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s",
+    name: "ReactJS",
+  },
+
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR01L6Jq6BvPIjmhTliTvigg2nAADj_Z-cfPw&s",
+    name: "SASS",
+  },
+  {
+    image:
+      "https://w7.pngwing.com/pngs/761/513/png-transparent-material-ui-logo.png",
+    name: "Material UI",
+  },
+  {
+    image:
+      "https://w7.pngwing.com/pngs/293/485/png-transparent-tailwind-css-hd-logo.png",
+    name: "Tailwind CSS",
+  },
+
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKonqa9PQ5az3aGx6-H7f7gyQOuIOBwGMElg&s",
+    name: "Redux",
+  },
+];
+
+export const backEndDevelopments = [
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYrNVwxlBhUzwBd6H4YqdJckckasr4N6PjQw&s",
+    name: "NodeJ",
+  },
+
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKonqa9PQ5az3aGx6-H7f7gyQOuIOBwGMElg&s",
+    name: "Redux",
+  },
+];
+
+export const mobileAppDevelopments = [
+  {
+    image:
+      "https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png",
+    name: "React Native",
+  },
+];
