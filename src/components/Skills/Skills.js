0a1fc34138ef75ecb647d1334f299c58e2621038
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-29 19:18:15 +0700

    create component render Skill UI

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,63 @@
+import { Avatar, Box, IconButton, Tooltip, Typography } from "@mui/material";
+import React from "react";
+import TagIcon from "@mui/icons-material/Tag";
+import { CustomizeTypography } from "../CustomizeTypography/CustomizeTypography";
+import { theme } from "../../theme/theme";
+
+function Skills({ skill, data }) {
+  return (
+    <Box sx={{ mb: 2 }}>
+      <Box>
+        <CustomizeTypography
+          sx={{
+            fontSize: "24px",
+            color: theme.palette.secondaryColor,
+            fontWeight: "bold",
+          }}
+        >
+          <IconButton sx={{ padding: 0 }}>
+            <TagIcon
+              sx={{ fontSize: "24px", color: theme.palette.secondaryColor }}
+            />
+          </IconButton>
+          {skill}
+        </CustomizeTypography>
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        {data.map((language, index) => (
+          <Tooltip
+            key={index}
+            title={
+              <Typography
+                sx={{
+                  fontSize: "13px",
+                  mb: 0,
+                }}
+              >
+                {language.name}
+              </Typography>
+            }
+          >
+            <Avatar
+              src={language.image}
+              alt={language.name}
+              sx={{
+                mt: 1,
+                mr: 2,
+                height: 56,
+                width: 56,
+                objectFit: "cover",
+                // borderRadius: 0,
+                "&:hover": {
+                  cursor: "pointer",
+                },
+              }}
+            />
+          </Tooltip>
+        ))}
+      </Box>
+    </Box>
+  );
+}
+
+export default Skills;
