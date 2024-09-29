aa7a2f5891f6140fd119f7f0a1ef8c1b16d063dc
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-29 19:17:55 +0700

    add skills for about

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,21 @@
-import { Box } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import React from "react";
 import { CustomizeTypography } from "./CustomizeTypography/CustomizeTypography";
+import TagIcon from "@mui/icons-material/Tag";
+import { theme } from "../theme/theme";
+import Skills from "./Skills/Skills";
+import {
+  languagesData,
+  databaseData,
+  frontEndDevelopments,
+  backEndDevelopments,
+  mobileAppDevelopments,
+} from "../data/skillsData";
 
 function About() {
   return (
     <Box>
-      <CustomizeTypography
-        sx={{ fontSize: "32px", color: "yellowgreen", fontWeight: "bold" }}
-      >
-        About me.
-      </CustomizeTypography>
+      <AboutTitle title={"About me"} />
       <CustomizeTypography sx={{ fontSize: "18px" }}>
         I am a student from HCMC University of Technology and Education.
       </CustomizeTypography>
@@ -17,8 +23,47 @@ function About() {
         Currently, I am working on my graduation project to prepare for
         graduation.
       </CustomizeTypography>
+
+      <Box sx={{ mt: 2 }}>
+        <AboutTitle title={"Skills"} />
+        <Skills skill={"Languages"} data={languagesData} />
+        <Skills skill={"Database"} data={databaseData} />
+        <Skills skill={"Front-end Developments"} data={frontEndDevelopments} />
+        <Skills skill={"Back-end Developments"} data={backEndDevelopments} />
+        <Skills
+          skill={"Mobile App Developments"}
+          data={mobileAppDevelopments}
+        />
+      </Box>
     </Box>
   );
 }
 
 export default About;
+
+const AboutTitle = ({ title }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <CustomizeTypography
+        sx={{ fontSize: "32px", color: "yellowgreen", fontWeight: "bold" }}
+      >
+        {title}
+        {/* About me */}
+      </CustomizeTypography>
+      <Box
+        sx={{
+          ml: 2,
+          mt: 1,
+          height: 4,
+          width: "180px",
+          bgcolor: "yellowgreen",
+        }}
+      />
+    </Box>
+  );
+};
