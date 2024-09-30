9f335138679553644fec7943dd8c0a802d3655d5
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-30 09:46:14 +0700

    need to fix for image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { Avatar, Box } from "@mui/material";
+import { Avatar, Box, Container } from "@mui/material";
 import React from "react";
 import { CustomizeTypography } from "./CustomizeTypography/CustomizeTypography";
 import { mobileScreen, theme } from "../theme/theme";
@@ -7,57 +7,59 @@ import Grid from "@mui/material/Grid2";
 
 function Home() {
   return (
-    <Grid
-      container
-      sx={{
-        mt: 10,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        [mobileScreen]: {
-          flexDirection: "column",
+    <Container>
+      <Grid
+        container
+        sx={{
+          mt: 10,
+          display: "flex",
           alignItems: "center",
           justifyContent: "center",
-        },
-      }}
-    >
-      <Grid item size={{ xs: 12, sm: 6, md: 6, lg: 6 }}>
-        <Box>
-          <CustomizeTypography sx={{ fontSize: 28, fontWeight: "600" }}>
-            Hello I'm Khoa,
-          </CustomizeTypography>
-          <CustomizeTypography
+          [mobileScreen]: {
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          },
+        }}
+      >
+        <Grid item size={{ xs: 12, sm: 6, md: 6, lg: 6 }}>
+          <Box>
+            <CustomizeTypography sx={{ fontSize: 28, fontWeight: "600" }}>
+              Hello I'm Khoa,
+            </CustomizeTypography>
+            <CustomizeTypography
+              sx={{
+                fontSize: 100,
+                color: theme.palette.secondaryColor,
+                fontWeight: "bold",
+                width: "100%",
+                [mobileScreen]: {
+                  fontSize: 50,
+                },
+              }}
+            >
+              Front-end Developer
+            </CustomizeTypography>
+          </Box>
+        </Grid>
+        <Grid item size={{ xs: 12, sm: 6, md: 6, lg: 6 }}>
+          <Avatar
+            src={back}
+            alt="My Image"
             sx={{
-              fontSize: 100,
-              color: theme.palette.secondaryColor,
-              fontWeight: "bold",
+              objectFit: "cover",
+              height: "100%",
               width: "100%",
+              borderRadius: "31% 69% 25% 75% / 70% 35% 65% 30%",
               [mobileScreen]: {
-                fontSize: 50,
+                width: "80%",
+                height: "80%",
               },
             }}
-          >
-            Front-end Developer
-          </CustomizeTypography>
-        </Box>
+          />
+        </Grid>
       </Grid>
-      <Grid item size={{ xs: 12, sm: 6, md: 6, lg: 6 }}>
-        <Avatar
-          src={back}
-          alt="My Image"
-          sx={{
-            objectFit: "cover",
-            height: "100%",
-            width: "100%",
-            borderRadius: "31% 69% 25% 75% / 70% 35% 65% 30%",
-            [mobileScreen]: {
-              width: "80%",
-              height: "80%",
-            },
-          }}
-        />
-      </Grid>
-    </Grid>
+    </Container>
   );
 }
 
