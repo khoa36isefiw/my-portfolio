b1e86db6c5ed5cf830899fcd06010698d4e6b887
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-10-12 18:12:23 +0700

    define namespace

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,13 @@
 import i18n from "i18next";
+// English
 import HOME_EN from "./locales/en/home.en.json";
+import HEADER_EN from "./locales/en/header.en.json";
+import ABOUT_EN from "./locales/en/about.en.json";
+// Vietnamese
 import HOME_VI from "./locales/vi/home.vi.json";
+import HEADER_VI from "./locales/vi/header.vi.json";
+import ABOUT_VI from "./locales/vi/about.vi.json";
+
 export const locales = {
   en: "English",
   vi: "Vietnamese",
@@ -14,16 +21,20 @@ i18n.init({
   interpolation: {
     escapeValue: false,
   },
-  ns: ["home"],
+  ns: ["home", "header", "about"],
   defaultNS,
   resources: {
     en: {
       // namespace
+      header: HEADER_EN,
       home: HOME_EN,
+      about: ABOUT_EN,
     },
     vi: {
       // namespace
+      header: HEADER_VI,
       home: HOME_VI,
+      about: ABOUT_VI,
     },
   },
 });
