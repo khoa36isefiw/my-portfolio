e68b0c703c31083a73651b1f77f4b74f8176d4ea
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-10-12 18:13:04 +0700

    define routes for multiple languages

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,37 @@
+import { Navigate, useLocation, useParams } from "react-router-dom";
 import Blog from "./components/Blog/Blog";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import DefaultLayout from "./layouts/DefaultLayout";
 
-// not sign into this website
+// // not sign into this website
+// const publicRoutes = [
+//   { path: "/en", component: Home, layout: DefaultLayout },
+//   { path: "/en/blog", component: Blog, layout: DefaultLayout },
+
+//   { path: "/vi", component: Home, layout: DefaultLayout },
+//   { path: "/vi/blog", component: Blog, layout: DefaultLayout },
+
+//   {
+//     path: "/my-projects",
+//     component: Projects,
+//     layout: DefaultLayout,
+//   },
+// ];
+
 const publicRoutes = [
-  { path: "/en", component: Home, layout: DefaultLayout },
-  { path: "/vi", component: Home, layout: DefaultLayout },
-  { path: "/blog", component: Blog, layout: DefaultLayout },
   {
-    path: "/my-projects",
+    path: "/:lang",
+    component: Home,
+    layout: DefaultLayout,
+  },
+  {
+    path: "/:lang/blog",
+    component: Blog,
+    layout: DefaultLayout,
+  },
+  {
+    path: "/:lang/my-projects",
     component: Projects,
     layout: DefaultLayout,
   },
