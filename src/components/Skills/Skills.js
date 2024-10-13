b043e772a4f2daf04776c592002bfb52bb4e6531
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-10-13 16:29:34 +0700

    change font family for list skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,7 +2,12 @@ import { Box, IconButton, Typography } from "@mui/material";
 import React from "react";
 import TagIcon from "@mui/icons-material/Tag";
 import { CustomizeTypography } from "../CustomizeTypography/CustomizeTypography";
-import { mobileScreen, tabletScreen, theme } from "../../theme/theme";
+import {
+  ipadProScreen,
+  mobileScreen,
+  tabletScreen,
+  theme,
+} from "../../theme/theme";
 
 function Skills({ skill, data }) {
   return (
@@ -11,7 +16,7 @@ function Skills({ skill, data }) {
         mb: 2,
         mr: 2,
 
-        minHeight: "250px",
+        minHeight: "280px",
         border: "1px solid #ccc",
         [tabletScreen]: {
           width: "100%",
@@ -23,13 +28,15 @@ function Skills({ skill, data }) {
       }}
     >
       {/* title for skill */}
-
       <Box sx={{ borderBottom: "1px solid #ccc" }}>
         <CustomizeTypography
           sx={{
             fontSize: "24px",
             color: theme.palette.secondaryColor,
             fontWeight: "bold",
+            [ipadProScreen]: {
+              fontSize: "20px",
+            },
           }}
         >
           <IconButton sx={{ padding: 0 }}>
@@ -40,12 +47,14 @@ function Skills({ skill, data }) {
           {skill}
         </CustomizeTypography>
       </Box>
+      {/* list skills */}
       <Box>
         {data.map((language, index) => (
           <ul key={index}>
             <li>
               <Typography
                 sx={{
+                  fontFamily: "Poppins",
                   fontSize: "16px",
                 }}
               >
